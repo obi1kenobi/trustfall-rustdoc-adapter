3ed8ad3d55dd15df0dfff0c5dd697bb137f9175a
Predrag Gruevski <obi1kenobi82@gmail.com>
2022-09-27 02:55:47 +0000

    Re-export the Crate type from rustdoc-types.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 mod adapter;
 mod indexed_crate;
 
+// Re-export the Crate type so we can deserialize it.
+pub use rustdoc_types::Crate;
+
 pub use {adapter::RustdocAdapter, indexed_crate::IndexedCrate};
