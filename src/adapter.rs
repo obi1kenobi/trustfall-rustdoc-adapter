2f9e8376d66acfaf0529a110002fa829dd16ad5c
Mieszko Grodzicki <mieszko.grodzicki@gmail.com>
2022-10-30 18:58:54 +0100

    Change &String to &str.

--- src/adapter.rs
+++ src/adapter.rs
@@ -135,10 +135,10 @@ pub struct Attribute {
     content: AttributeValue,
 }
 
-impl<'a> TryFrom<&'a String> for Attribute {    
+impl<'a> TryFrom<&'a str> for Attribute {    
     type Error = anyhow::Error;
 
-    fn try_from(as_string: &'a String) -> anyhow::Result<Self> {
+    fn try_from(as_string: &'a str) -> anyhow::Result<Self> {
         lazy_static! {
             static ref INNER_RE: Regex = Regex::new(r"#!\[(.*)\]").unwrap();
             static ref OUTER_RE: Regex = Regex::new(r"#\[(.*)\]").unwrap();
@@ -146,12 +146,12 @@ impl<'a> TryFrom<&'a String> for Attribute {
         match INNER_RE.captures(as_string) {
             Some(captures) => Ok(Attribute {
                 is_inner: true,
-                content: AttributeValue::try_from(&captures[1].to_string())?,
+                content: AttributeValue::try_from(&captures[1])?,
             }),
             None => match OUTER_RE.captures(as_string) {
                 Some(captures) => Ok(Attribute {
                     is_inner: false,
-                    content: AttributeValue::try_from(&captures[1].to_string())?,
+                    content: AttributeValue::try_from(&captures[1])?,
                 }),
                 None => bail!("Attribute has to be in one of the following forms: `#[...]` or `#![...]`, but found: `{}`", as_string)
             }
@@ -167,12 +167,12 @@ pub struct AttributeValue {
     arguments: Option<Vec<AttributeValue>>,
 }
 
-impl<'a> TryFrom<&'a String> for AttributeValue {
+impl<'a> TryFrom<&'a str> for AttributeValue {
     type Error = anyhow::Error;
 
     // TODO: Are we sure it recognises all possible attributes? Maybe it's better
     // to ignore errors instead of panicing?
-    fn try_from(as_string: &'a String) -> anyhow::Result<Self> {
+    fn try_from(as_string: &'a str) -> anyhow::Result<Self> {
         const PATH_RE_STR: &str = r"[[:blank:]]*(?P<simple_path>[[:word:]:]+)[[:blank:]]*";
         lazy_static! {
             static ref PATH_RE: Regex = Regex::new(format!(r"^{}$", PATH_RE_STR).as_str()).unwrap();
@@ -181,12 +181,12 @@ impl<'a> TryFrom<&'a String> for AttributeValue {
         }
 
         PATH_RE.captures(as_string).map(|captures| AttributeValue {
-            as_string: as_string.clone(),
+            as_string: as_string.to_string(),
             base: captures["simple_path"].to_string(),
             assigned_expression: None,
             arguments: None,
         }).or_else(|| ASSIGNMENT_RE.captures(as_string).map(|captures| AttributeValue {
-            as_string: as_string.clone(),
+            as_string: as_string.to_string(),
             base: captures["simple_path"].to_string(),
             assigned_expression: Some(captures["expression"].to_string()),
             arguments: None,
@@ -206,17 +206,17 @@ impl<'a> TryFrom<&'a String> for AttributeValue {
                 } else if c == ')' || c == ']' || c == '}' {
                     depth -= 1;
                 } else if c == ',' && depth == 0 {
-                    arguments.push(AttributeValue::try_from(&arg_str[i..j].to_string()).unwrap());
+                    arguments.push(AttributeValue::try_from(&arg_str[i..j]).unwrap());
                     i = j + 1;
                     only_white = true;
                 }
             }
             if i < arg_str.len() && !only_white {
-                arguments.push(AttributeValue::try_from(&arg_str[i..].to_string()).unwrap());
+                arguments.push(AttributeValue::try_from(&arg_str[i..]).unwrap());
             }
 
             AttributeValue {
-                as_string: as_string.clone(),
+                as_string: as_string.to_string(),
                 base: captures["simple_path"].to_string(),
                 assigned_expression: None,
                 arguments: Some(arguments),
@@ -922,7 +922,7 @@ impl<'a> Adapter<'a> for RustdocAdapter<'a> {
                                     Box::new(
                                         item.attrs
                                             .iter()
-                                            .map(move |attr| origin.make_attribute_token(Attribute::try_from(attr).unwrap())),
+                                            .map(move |attr| origin.make_attribute_token(Attribute::try_from(attr.as_str()).unwrap())),
                                     )
                                 }
                             };
@@ -1418,7 +1418,7 @@ mod tests {
 
     #[test]
     fn attribute_from_string_simple_inner() {
-        let attribute = Attribute::try_from(&"#![no_std]".to_string()).unwrap();
+        let attribute = Attribute::try_from("#![no_std]").unwrap();
         assert_eq!(attribute, Attribute {
             is_inner: true,
             content: AttributeValue {
@@ -1432,7 +1432,7 @@ mod tests {
 
     #[test]
     fn attribute_from_string_complex_outer() {
-        let attribute = Attribute::try_from(&"#[cfg_attr(feature = \"serde\", derive(Serialize, Deserialize))]".to_string()).unwrap();
+        let attribute = Attribute::try_from("#[cfg_attr(feature = \"serde\", derive(Serialize, Deserialize))]").unwrap();
         assert_eq!(attribute, Attribute {
             is_inner: false,
             content: AttributeValue {
@@ -1473,7 +1473,7 @@ mod tests {
     #[test]
     fn attribute_value_from_string_custom_brackets() {
         for as_string in ["macro{arg1,arg2}", "macro[arg1,arg2]"] {
-            let attr_val = AttributeValue::try_from(&as_string.to_string()).unwrap();
+            let attr_val = AttributeValue::try_from(as_string).unwrap();
             assert_eq!(attr_val, AttributeValue {
                 as_string: as_string.to_string(),
                 base: "macro".to_string(),
