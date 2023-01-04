bd0a641b1e4c45562df04d77ab241b8854017b15
Tomasz Nowak <36604952+tonowak@users.noreply.github.com>
2023-01-04 17:07:40 +0100

    Fix rustdoc trait incompatibility (#24)
    * Fix rustdoc trait incompatibility
    
    * First reviewable version
    
    * clippy
    
    * Update src/indexed_crate.rs
    
    Co-authored-by: Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
    
    * fmt
    
    * Change an edge in schema to optional
    
    * Changed name from dummy to manually_inlined_builtin_traits
    
    * Searching for the ID on the origin data, instead of both
    
    * Extra bonus points
    
    * Changed comment as suggested
    
    * fmt
    
    * Update src/indexed_crate.rs
    
    Co-authored-by: Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
    
    * Moved manual trait list to a const of a struct
    
    * Filling crate_id with correct value
    
    * Update src/indexed_crate.rs
    
    Co-authored-by: Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
    
    * Update src/indexed_crate.rs
    
    Co-authored-by: Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
    
    Co-authored-by: Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>

--- src/indexed_crate.rs
+++ src/indexed_crate.rs
@@ -2,21 +2,40 @@ use std::collections::HashMap;
 
 use rustdoc_types::{Crate, Id, Item, Visibility};
 
+/// The rustdoc for a crate, together with associated indexed data to speed up common operations.
+///
+/// Besides the parsed rustdoc, it also contains some manually-inlined `rustdoc_types::Trait`s
+/// of the most common built-in traits.
+/// This is a temporary step, until we're able to combine rustdocs of multiple crates.
 #[derive(Debug, Clone)]
 pub struct IndexedCrate<'a> {
     pub(crate) inner: &'a Crate,
 
     // For an Id, give the list of item Ids under which it is publicly visible.
     pub(crate) visibility_forest: HashMap<&'a Id, Vec<&'a Id>>,
+
+    /// Trait items defined in external crates are not present in the `inner: &Crate` field,
+    /// even if they are implemented by a type in that crate. This also includes
+    /// Rust's built-in traits like `Debug, Send, Eq` etc.
+    ///
+    /// This change is approximately as of rustdoc v23,
+    /// in https://github.com/rust-lang/rust/pull/105182
+    ///
+    /// As a temporary workaround, we manually create the trait items
+    /// for the most common Rust built-in traits and link to those items
+    /// as if they were still part of the rustdoc JSON file.
+    ///
+    /// A more complete future solution may generate multiple crates' rustdoc JSON
+    /// and link to the external crate's trait items as necessary.
+    pub(crate) manually_inlined_builtin_traits: HashMap<Id, Item>,
 }
 
 impl<'a> IndexedCrate<'a> {
     pub fn new(crate_: &'a Crate) -> Self {
-        let visibility_forest = calculate_visibility_forest(crate_);
-
         Self {
             inner: crate_,
-            visibility_forest,
+            visibility_forest: calculate_visibility_forest(crate_),
+            manually_inlined_builtin_traits: create_manually_inlined_builtin_traits(crate_),
         }
     }
 
@@ -147,4 +166,144 @@ fn collect_public_items<'a>(
         }
         Visibility::Crate | Visibility::Restricted { .. } => {}
     }
+}
+
+#[derive(Debug)]
+struct ManualTraitItem {
+    name: &'static str,
+    is_auto: bool,
+    is_unsafe: bool,
+}
+
+/// Limiting the creation of manually inlined traits to only those that are used by the lints.
+/// There are other foreign traits, but it is not obvious how the manually inlined traits
+/// should look like for them.
+const MANUAL_TRAIT_ITEMS: [ManualTraitItem; 14] = [
+    ManualTraitItem {
+        name: "Debug",
+        is_auto: false,
+        is_unsafe: false,
+    },
+    ManualTraitItem {
+        name: "Clone",
+        is_auto: false,
+        is_unsafe: false,
+    },
+    ManualTraitItem {
+        name: "Copy",
+        is_auto: false,
+        is_unsafe: false,
+    },
+    ManualTraitItem {
+        name: "PartialOrd",
+        is_auto: false,
+        is_unsafe: false,
+    },
+    ManualTraitItem {
+        name: "Ord",
+        is_auto: false,
+        is_unsafe: false,
+    },
+    ManualTraitItem {
+        name: "PartialEq",
+        is_auto: false,
+        is_unsafe: false,
+    },
+    ManualTraitItem {
+        name: "Eq",
+        is_auto: false,
+        is_unsafe: false,
+    },
+    ManualTraitItem {
+        name: "Hash",
+        is_auto: false,
+        is_unsafe: false,
+    },
+    ManualTraitItem {
+        name: "Send",
+        is_auto: true,
+        is_unsafe: true,
+    },
+    ManualTraitItem {
+        name: "Sync",
+        is_auto: true,
+        is_unsafe: true,
+    },
+    ManualTraitItem {
+        name: "Unpin",
+        is_auto: true,
+        is_unsafe: false,
+    },
+    ManualTraitItem {
+        name: "RefUnwindSafe",
+        is_auto: true,
+        is_unsafe: false,
+    },
+    ManualTraitItem {
+        name: "UnwindSafe",
+        is_auto: true,
+        is_unsafe: false,
+    },
+    ManualTraitItem {
+        name: "Sized",
+        is_auto: false,
+        is_unsafe: false,
+    },
+];
+
+fn new_trait(manual_trait_item: &ManualTraitItem, id: Id, crate_id: u32) -> Item {
+    Item {
+        id,
+        crate_id,
+        name: Some(manual_trait_item.name.to_string()),
+        span: None,
+        visibility: rustdoc_types::Visibility::Public,
+        docs: None,
+        links: HashMap::new(),
+        attrs: Vec::new(),
+        deprecation: None,
+        inner: rustdoc_types::ItemEnum::Trait(rustdoc_types::Trait {
+            is_auto: manual_trait_item.is_auto,
+            is_unsafe: manual_trait_item.is_unsafe,
+            // The `item`, `generics`, `bounds` and `implementations`
+            // are not currently present in the schema,
+            // so it is safe to fill them with empty containers,
+            // even though some traits in reality have some values in them.
+            items: Vec::new(),
+            generics: rustdoc_types::Generics {
+                params: Vec::new(),
+                where_predicates: Vec::new(),
+            },
+            bounds: Vec::new(),
+            implementations: Vec::new(),
+        }),
+    }
+}
+
+fn create_manually_inlined_builtin_traits(crate_: &Crate) -> HashMap<Id, Item> {
+    let paths = crate_
+        .index
+        .values()
+        .map(|item| &item.inner)
+        .filter_map(|item_enum| match item_enum {
+            rustdoc_types::ItemEnum::Impl(impl_) => Some(impl_),
+            _ => None,
+        })
+        .filter_map(|impl_| impl_.trait_.as_ref());
+
+    paths
+        .filter_map(|path| {
+            MANUAL_TRAIT_ITEMS
+                .iter()
+                .find(|manual| manual.name == path.name)
+                .and_then(|manual| {
+                    crate_.paths.get(&path.id).map(|item_summary| {
+                        (
+                            path.id.clone(),
+                            new_trait(manual, path.id.clone(), item_summary.crate_id),
+                        )
+                    })
+                })
+        })
+        .collect()
 }
