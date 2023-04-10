4f1164bedfb5f6e47836779f60bd5cba651cf39d
Predrag Gruevski <obi1kenobi82@gmail.com>
2023-04-10 23:05:37 +0000

    Fix.

--- src/adapter.rs
+++ src/adapter.rs
@@ -1217,13 +1217,18 @@ impl<'a> Adapter<'a> for RustdocAdapter<'a> {
 
 #[cfg(test)]
 mod tests {
+    use anyhow::Context;
+
     #[test]
     fn rustdoc_json_format_version() {
+        let path = "./localdata/test_data/reexport/rustdoc.json";
+        let content = std::fs::read_to_string(path)
+            .with_context(|| format!("Could not load {path} file, did you forget to run ./scripts/regenerate_test_rustdocs.sh ?"))
+            .expect("failed to load rustdoc");
+
         let expected_version = rustdoc_types::FORMAT_VERSION;
-        let actual_version = crate::test_util::detect_rustdoc_format_version(
-            "./localdata/test_data/reexport/rustdoc.json",
-        )
-        .expect("unrecognized rustdoc format");
+        let actual_version = crate::test_util::detect_rustdoc_format_version(&content)
+            .expect("unrecognized rustdoc format");
 
         assert_eq!(
             expected_version, actual_version,
