d837bbbe8a83e5332c7210be51eadf22d1d0a530
Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
2023-04-10 19:07:17 -0400

    Use `&self` instead of `&mut self` in `impl Adapter`. (#128) (#129)

--- src/adapter.rs
+++ src/adapter.rs
@@ -608,7 +608,7 @@ impl<'a> Adapter<'a> for RustdocAdapter<'a> {
     type Vertex = Vertex<'a>;
 
     fn resolve_starting_vertices(
-        &mut self,
+        &self,
         edge_name: &Arc<str>,
         _parameters: &EdgeParameters,
         _resolve_info: &ResolveInfo,
@@ -630,7 +630,7 @@ impl<'a> Adapter<'a> for RustdocAdapter<'a> {
     }
 
     fn resolve_property(
-        &mut self,
+        &self,
         contexts: ContextIterator<'a, Self::Vertex>,
         type_name: &Arc<str>,
         property_name: &Arc<str>,
@@ -688,7 +688,7 @@ impl<'a> Adapter<'a> for RustdocAdapter<'a> {
     }
 
     fn resolve_neighbors(
-        &mut self,
+        &self,
         contexts: ContextIterator<'a, Self::Vertex>,
         type_name: &Arc<str>,
         edge_name: &Arc<str>,
@@ -1175,7 +1175,7 @@ impl<'a> Adapter<'a> for RustdocAdapter<'a> {
     }
 
     fn resolve_coercion(
-        &mut self,
+        &self,
         contexts: ContextIterator<'a, Self::Vertex>,
         type_name: &Arc<str>,
         coerce_to_type: &Arc<str>,
