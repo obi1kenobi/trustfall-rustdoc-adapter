d54b371de8dfe747bb13844fc793e5e7e1fd4e5c
Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
2023-04-21 16:58:44 -0400

    Ensure that items within `impl Trait for &Foo` work properly.  (#163)
    * Add `impl_for_ref` test crate.
    
    * Add test case that checks all 3 methods are found.
    
    * Cargo fmt.

--- src/adapter/tests.rs
+++ src/adapter/tests.rs
@@ -1,4 +1,10 @@
+use std::{rc::Rc, sync::Arc};
+
 use anyhow::Context;
+use maplit::btreemap;
+use trustfall::{FieldValue, Schema};
+
+use crate::{IndexedCrate, RustdocAdapter};
 
 #[test]
 fn rustdoc_json_format_version() {
@@ -14,5 +20,53 @@ fn rustdoc_json_format_version() {
     assert_eq!(
         expected_version, actual_version,
         "Expected to find rustdoc v{expected_version} but got v{actual_version} instead.",
+    );
+}
+
+/// Ensure that methods implemented on references (like `&Foo`) show up in queries.
+#[test]
+fn impl_for_ref() {
+    let path = "./localdata/test_data/impl_for_ref/rustdoc.json";
+    let content = std::fs::read_to_string(path)
+        .with_context(|| format!("Could not load {path} file, did you forget to run ./scripts/regenerate_test_rustdocs.sh ?"))
+        .expect("failed to load rustdoc");
+
+    let crate_ = serde_json::from_str(&content).expect("failed to parse rustdoc");
+    let indexed_crate = IndexedCrate::new(&crate_);
+    let adapter = RustdocAdapter::new(&indexed_crate, None);
+
+    let query = r#"
+{
+    Crate {
+        item {
+            ... on Struct {
+                name @filter(op: "=", value: ["$struct"])
+
+                impl @fold @transform(op: "count") @output(name: "matching_methods") {
+                    method {
+                        name @filter(op: "=", value: ["$method"])
+                    }
+                }
+            }
+        }
+    }
+}
+"#;
+    let variables = btreemap! {
+        "struct" => "StringHolder",
+        "method" => "eq",
+    };
+
+    let schema =
+        Schema::parse(include_str!("../rustdoc_schema.graphql")).expect("schema failed to parse");
+    let results: Vec<_> = trustfall::execute_query(&schema, Rc::new(adapter), query, variables)
+        .expect("failed to run query")
+        .collect();
+
+    assert_eq!(
+        vec![btreemap! {
+            Arc::from("matching_methods") => FieldValue::Uint64(3),
+        }],
+        results
     );
 }
