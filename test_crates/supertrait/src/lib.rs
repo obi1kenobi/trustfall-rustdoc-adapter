76f6600519c732baf73b498b4e22b03abe22dfa1
Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
2024-09-08 11:52:30 -0400

    Allow `supertrait` edge to resolve to common built-in traits too. (#464)

--- test_crates/supertrait/src/lib.rs
+++ test_crates/supertrait/src/lib.rs
@@ -2,4 +2,9 @@ pub trait Supertrait {}
 
 pub trait Supertrait2 {}
 
-pub trait BaseTrait : Supertrait2 + Supertrait {}
+pub trait MyTrait : Supertrait2 + Supertrait {}
+
+// Ensure `std::fmt::Debug` is a supertrait here, verbatim.
+// It's a load-bearing part of the test, since we want our query output names to be invariant
+// to how they are written down in the source.
+pub trait DebugPartialOrd : std::fmt::Debug + PartialOrd {}
