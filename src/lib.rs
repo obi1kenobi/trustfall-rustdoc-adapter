95db43d609243059fcbc7946f295f1321dcf8a78
Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
2024-11-30 13:11:39 -0500

    Add support for querying manifest information. (#628) (#634)
    - **Add schema for features.**
    - **Implement feature schema in the Trustfall adapter.**
    - **Move `Adapter` impl to `&RustdocAdapter` and add `Drop` impl.**
    - **Add support for querying manifest information.**

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,10 @@ pub(crate) mod test_util;
 // Re-export the Crate type so we can deserialize it.
 pub use rustdoc_types::Crate;
 
+// Re-export `cargo_metadata` since its types are in our public API.
+pub use cargo_metadata;
+
 pub use {
     adapter::RustdocAdapter,
-    indexed_crate::{ImportablePath, IndexedCrate},
+    indexed_crate::{ImportablePath, IndexedCrate, PackageIndex, PackageStorage},
 };
