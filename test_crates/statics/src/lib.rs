062907e6eb9605dce5924ed54389d0d89811bb10
Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
2025-01-27 15:49:40 -0500

    add `unsafe` to static (#725) (#730)
    close: #724
    
    Should I maybe rename the statics?
    
    ---------
    
    Co-authored-by: frankwang <73262844+Frank-III@users.noreply.github.com>

--- test_crates/statics/src/lib.rs
+++ test_crates/statics/src/lib.rs
@@ -1,7 +1,14 @@
+#![feature(unsafe_extern_blocks)]
+
 pub static FIRST: u32 = 1;
 
 pub static mut MUT: i64 = 0;
 
 pub mod inner {
     pub static SECOND: i64 = 2;
+}
+
+unsafe extern "C" {
+    pub safe static SAFE: i64;
+    pub unsafe static UNSAFE: i64;
 }
