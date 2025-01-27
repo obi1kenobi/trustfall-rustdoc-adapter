9f648280ec6a0757c3047c3d9d08034aa4012000
Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
2025-01-27 11:19:55 -0500

    add `dyn_compatible` field to `Trait` data model (#718) (#720)
    close part of #535
    
    ---------
    
    Co-authored-by: Predrag Gruevski
    <2348618+obi1kenobi@users.noreply.github.com>
    
    Co-authored-by: frankwang <73262844+Frank-III@users.noreply.github.com>

--- src/adapter/properties.rs
+++ src/adapter/properties.rs
@@ -446,7 +446,7 @@ pub(super) fn resolve_trait_property<'a, V: AsVertex<Vertex<'a>> + 'a>(
 ) -> ContextOutcomeIterator<'a, V, FieldValue> {
     match property_name {
         "unsafe" => resolve_property_with(contexts, field_property!(as_trait, is_unsafe)),
-        "object_safe" => {
+        "object_safe" | "dyn_compatible" => {
             resolve_property_with(contexts, field_property!(as_trait, is_dyn_compatible))
         }
         "sealed" => resolve_property_with(contexts, move |vertex| {
