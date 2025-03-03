78b62d51fb6711de9fdf6ebc090db2ac61da41b1
Predrag Gruevski <obi1kenobi82@gmail.com>
2025-03-03 04:17:52 +0000

    Ensure we parse the `#[non_exhaustive]` attribute correctly.

---
+++ test_crates/non_exhaustive_attribute/src/lib.rs
@@ -0,0 +1,14 @@
+#[non_exhaustive]
+pub enum NonExhaustiveEnum {
+    First,
+}
+
+#[non_exhaustive]
+pub struct NonExhaustive {
+    pub x: i64,
+}
+
+pub enum MyEnum {
+    #[non_exhaustive]
+    NonExhaustiveVariant(i64),
+}
