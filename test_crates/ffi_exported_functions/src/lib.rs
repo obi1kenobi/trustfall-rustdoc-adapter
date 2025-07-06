89a0218d2d7040e89e430969734a9b976dacfd18
Predrag Gruevski <obi1kenobi82@gmail.com>
2025-07-06 15:14:27 +0000

    Migrate the crate and its test crates to edition 2024.

--- test_crates/ffi_exported_functions/src/lib.rs
+++ test_crates/ffi_exported_functions/src/lib.rs
@@ -1,19 +1,19 @@
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "C" fn top_level_no_mangle_fn() {}
 
-#[export_name = "exported"]
+#[unsafe(export_name = "exported")]
 pub extern "C-unwind" fn top_level_export_name_fn() {}
 
 #[repr(C)]
 pub struct Example;
 
 impl Example {
-    #[no_mangle]
+    #[unsafe(no_mangle)]
     pub extern "C" fn associated_fn() {}
 
-    #[export_name = "assoc_exported"]
+    #[unsafe(export_name = "assoc_exported")]
     pub extern "C-unwind" fn assoc_exported_fn() {}
 
-    #[no_mangle]
+    #[unsafe(no_mangle)]
     pub extern "C" fn method(&self) {}
 }
