90ffd11baed016a03871fa20e633eedc1bdd907b
CLIDragon <84266961+CLIDragon@users.noreply.github.com>
2025-07-10 11:32:22 +1000

    cargo fmt

--- src/hashtables.rs
+++ src/hashtables.rs
@@ -8,4 +8,4 @@ pub(crate) use rustc_hash::{FxHashMap as HashMap, FxHashSet as HashSet};
 pub(crate) type IndexMap<K, V> = indexmap::map::IndexMap<K, V, rustc_hash::FxBuildHasher>;
 
 #[cfg(not(feature = "rustc-hash"))]
-pub(crate) use indexmap::{map::IndexMap};
+pub(crate) use indexmap::map::IndexMap;
