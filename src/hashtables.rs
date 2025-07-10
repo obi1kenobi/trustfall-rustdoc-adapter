79deee58375237272ea11ca49798e5c2467f499f
CLIDragon <84266961+CLIDragon@users.noreply.github.com>
2025-07-10 11:27:44 +1000

    Export IndexSet in hashtables.rs

--- src/hashtables.rs
+++ src/hashtables.rs
@@ -7,5 +7,8 @@ pub(crate) use rustc_hash::{FxHashMap as HashMap, FxHashSet as HashSet};
 #[cfg(feature = "rustc-hash")]
 pub(crate) type IndexMap<K, V> = indexmap::map::IndexMap<K, V, rustc_hash::FxBuildHasher>;
 
+#[cfg(feature = "rustc-hash")]
+pub(crate) type IndexSet<T> = indexmap::map::IndexSet<T, rustc_hash::FxBuildHasher>;
+
 #[cfg(not(feature = "rustc-hash"))]
-pub(crate) use indexmap::map::IndexMap;
+pub(crate) use indexmap::{map::IndexMap, set::IndexSet};
