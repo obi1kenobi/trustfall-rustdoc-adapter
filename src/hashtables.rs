0410c8f14eb6a055f68d4fcfcc69268d607507e6
Predrag Gruevski <2348618+obi1kenobi@users.noreply.github.com>
2025-07-10 22:04:45 -0400

    Add a per-item-kind index of public items (#902) (#907)
    This PR is based on #676. Due to merge conflicts it was easier to create
    a new PR than attempt to build on that one, but all the points made in
    that PR remain current.
    
    **Purpose of Change**
    From @obi1kenobi in #676:
    > Most items we'll see in the crate either aren't public, or aren't
    top-level items that would be returned by the `Crate -[item]-> Item`
    edge. For example, many of those items are methods, impls, trait
    associated types, associated constants, etc.
    >
    > In this PR, we ensure we don't bother iterating over such items in
    queries where they can't possibly match. Queries that look at public
    structs should only iterate over structs, ones that look at enums should
    only iterate over enums, etc.
    >
    > This has a substantial performance effect: it speeds up the median
    `cargo-semver-checks` lint by ~100x, and makes it so that a tiny handful
    (~5) of lints dominate all linting runtime — the combined runtime of all
    other lints becomes negligible. In end-to-end terms,
    `cargo-semver-checks` becomes about ~35% faster on our largest workload,
    but this number is misleadingly small w.r.t. the impact of this
    optimization: the new runtime is now equal to ~101% of the slowest lint,
    whereas that number was ~140% previously. If we can now optimize that
    slow lint in particular, we'll get tens of percent of additional speedup
    from the optimization in this PR.
    
    On top of that, when calling `resolve_crate_items` and resolving a path,
    all items matching that path are returned from `imports_index`. However,
    a single path might have dozens of imports, and only some of these items
    will be of the right type (e.g. a Trait or Enum).
    
    **Describe the Solution**
    In order to return only the items of the right type, we can take the
    intersection of `imports_index` and `pub_item_kind_index`. However, as
    both of these were vectors, it was very inefficient to find their
    intersection. Replacing the vectors inside `pub_item_kind_index` with
    maps allows efficiently taking the intersection of indices.
    
    As noted in
    
    https://github.com/obi1kenobi/trustfall-rustdoc-adapter/pull/676#issuecomment-3051767440,
    there is a decent improvement in performance thanks to this change
    (roughly a 40% improvement in total time spent in queries).
    Additionally, since the indices are generally quite small there is a
    negligible effect on indexed crate creation time.
    
    **Describe alternatives you've considered**
    - Use an `Rc<str>` instead of `Arc<str>`. I think it would be safe, but
    I don't know enough about trustfall's internals to be sure.
    - ~~Inline `PubItemKindIndex::contains` into
    `resolve_items_by_importable_path_field_value`~~. This was done.
    - Create the index in parallel. Benchmarks showed parallel construction
    used in the original commit was significantly slower than the
    single-threaded version, taking over a second compared to the
    single-threaded version resolving in milliseconds.
    
    ---------
    
    Co-authored-by: Predrag Gruevski <obi1kenobi82@gmail.com>
    Co-authored-by: Predrag Gruevski
    <2348618+obi1kenobi@users.noreply.github.com>
    
    Co-authored-by: CLIDragon <84266961+CLIDragon@users.noreply.github.com>

--- src/hashtables.rs
+++ src/hashtables.rs
@@ -1,5 +1,50 @@
+use std::default::Default;
+use std::hash::{BuildHasher, Hash};
+
 #[cfg(not(feature = "rustc-hash"))]
 pub(crate) use std::collections::{HashMap, HashSet};
 
 #[cfg(feature = "rustc-hash")]
 pub(crate) use rustc_hash::{FxHashMap as HashMap, FxHashSet as HashSet};
+
+#[cfg(feature = "rustc-hash")]
+pub(crate) type IndexMap<K, V> = indexmap::map::IndexMap<K, V, rustc_hash::FxBuildHasher>;
+
+#[cfg(not(feature = "rustc-hash"))]
+pub(crate) use indexmap::map::IndexMap;
+
+/// Allow using new() and with_capacity() regardless of the hash algorithm.
+/// See <https://github.com/tkaitchuck/aHash/issues/103> for more information.
+#[allow(dead_code, reason = "used when the `rustc-hash` feature is enabled")]
+pub(crate) trait HashMapExt {
+    fn new() -> Self;
+    fn with_capacity(x: usize) -> Self;
+}
+
+impl<K, V, S> HashMapExt for std::collections::HashMap<K, V, S>
+where
+    K: Hash + Eq,
+    S: BuildHasher + Default,
+{
+    fn new() -> Self {
+        std::collections::HashMap::with_hasher(S::default())
+    }
+
+    fn with_capacity(capacity: usize) -> Self {
+        std::collections::HashMap::with_capacity_and_hasher(capacity, S::default())
+    }
+}
+
+impl<K, V, S> HashMapExt for indexmap::map::IndexMap<K, V, S>
+where
+    K: Hash + Eq,
+    S: BuildHasher + Default,
+{
+    fn new() -> Self {
+        indexmap::map::IndexMap::with_hasher(S::default())
+    }
+
+    fn with_capacity(capacity: usize) -> Self {
+        indexmap::map::IndexMap::with_capacity_and_hasher(capacity, S::default())
+    }
+}
