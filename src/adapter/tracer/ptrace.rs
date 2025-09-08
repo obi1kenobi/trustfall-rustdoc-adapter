a55f7756121b29155417c0d3aa7df81608194306
CLIDragon <84266961+CLIDragon@users.noreply.github.com>
2025-09-08 20:47:04 +1000

    reduce collected information further

--- src/adapter/tracer/ptrace.rs
+++ src/adapter/tracer/ptrace.rs
@@ -15,74 +15,6 @@ use trustfall::{
     },
 };
 
-struct PreActionIter<T, I: Iterator<Item = T>, F: Fn()> {
-    inner: I,
-    pre_action: F,
-}
-
-impl<T, I, F> Iterator for PreActionIter<T, I, F>
-where
-    F: Fn(),
-    I: Iterator<Item = T>,
-{
-    type Item = T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        (self.pre_action)();
-        self.inner.next()
-    }
-}
-
-struct OnIterEnd<T, I: Iterator<Item = T>, F: FnOnce()> {
-    inner: I,
-    on_end_func: Option<F>,
-}
-
-impl<T, I, F> Iterator for OnIterEnd<T, I, F>
-where
-    F: FnOnce(),
-    I: Iterator<Item = T>,
-{
-    type Item = T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let result = self.inner.next();
-        if result.is_none() {
-            let end_func = self.on_end_func.take();
-            if let Some(func) = end_func {
-                func();
-            }
-        }
-        result
-    }
-}
-
-fn make_iter_with_pre_action<T, I: Iterator<Item = T>, F: Fn()>(
-    inner: I,
-    pre_action: F,
-) -> PreActionIter<T, I, F> {
-    PreActionIter { inner, pre_action }
-}
-
-fn make_iter_with_end_action<T, I: Iterator<Item = T>, F: FnOnce()>(
-    inner: I,
-    on_end: F,
-) -> OnIterEnd<T, I, F> {
-    OnIterEnd {
-        inner,
-        on_end_func: Some(on_end),
-    }
-}
-
-pub trait VertexT: Clone + Debug {}
-impl<T: Clone + Debug> VertexT for T {}
-
-/// The id of an operation.
-// PERF: NonZeroU32 is used instead of NonZeroUsize to reduce the size
-// of TraceOp by 8 bytes.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize)]
-pub struct Opid(pub NonZeroU32);
-
 /// Records and stores operations performed by the adapter.
 ///
 /// This struct is intended for use inside of a TracingAdapter.
@@ -90,86 +22,37 @@ pub struct Opid(pub NonZeroU32);
 /// Recording out-of-order operations will lead to invalid state.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Tracer {
-    pub ops: Vec<TraceOp>,
+    pub calls: BTreeMap<FunctionCall, Vec<Duration>>,
 }
 
 impl Tracer {
     /// Construct a new tracer.
     pub fn new() -> Self {
         Self {
-            ops: Vec::with_capacity(100_000),
+            calls: BTreeMap::new(),
         }
     }
 
     /// Record an operation.
-    pub fn record(
-        &mut self,
-        content: TraceOpType,
-        parent: Option<Opid>,
-        duration: Option<Duration>,
-    ) -> Opid {
-        let size_u32 =
-            u32::try_from(self.ops.len()).expect("operations should be smaller than u32::max") + 1;
-        let next_opid = Opid(NonZeroU32::new(size_u32).unwrap());
-
-        let op = TraceOp {
-            opid: next_opid,
-            parent_opid: parent,
-            duration: duration,
-            content,
-        };
-        self.ops.push(op);
-        next_opid
+    pub fn record_time(&mut self, call_id: FunctionCall, duration: Duration) {
+        self.calls
+            .entry(call_id)
+            .or_insert_with(|| Vec::with_capacity(1000))
+            .push(duration);
     }
 
-    pub fn operations(&self) -> &Vec<TraceOp> {
-        &self.ops
+    pub fn calls(&self) -> &BTreeMap<FunctionCall, Vec<Duration>> {
+        &self.calls
     }
 }
 
-/// An operation performed by the adapter.
-/// A parent_opid of None means that it is a top-level operation.
-/// A TraceOp is only intended to be constructed by a Tracer.
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct TraceOp {
-    pub opid: Opid,                 // 4 bytes
-    pub parent_opid: Option<Opid>,  // 4 bytes
-    pub duration: Option<Duration>, // 16 bytes (12 + alignment)
-    pub content: TraceOpType,       // 8 bytes
-}
-
-/// The type of an operation.
-/// Each type corresponds to a particular operation performed by an adaper.
-/// Call: A function is called.
-/// AdvanceInputIterator: An iterator input to a function is incremented.
-/// YieldInto: An input function returns a value. Called from the function
-///     which takes input.
-/// YieldFrom: A function returns a value. Called from inside the function.
-/// InputIteratorExhausted: No further inputs available
-/// OutputIteratorExhausted: No further outputs available
-/// ProduceQueryResult: A result is returned from a query.
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum TraceOpType {
-    // FunctionCall is boxed to reduce the size of TraceOpType.
-    Call(Box<FunctionCall>),
-    YieldFrom(YieldValue),
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum YieldValue {
-    ResolveStartingVertices,
-    ResolveProperty,
-    ResolveNeighborsOuter,
-    ResolveNeighborsInner,
-    ResolveCoercion,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum FunctionCall {
-    ResolveStartingVertices(Vid),             // vertex ID
-    ResolveProperty(Vid, Arc<str>, Arc<str>), // vertex ID + type name + name of the property
-    ResolveNeighbors(Vid, Arc<str>, Eid),     // vertex ID + type name + edge ID
-    ResolveCoercion(Vid, Arc<str>, Arc<str>), // vertex ID + current type + coerced-to type
+    ResolveStartingVertices(Vid),              // vertex ID
+    ResolveProperty(Vid, Arc<str>, Arc<str>),  // vertex ID + type name + name of the property
+    ResolveNeighbors(Vid, Arc<str>, Eid),      // vertex ID + type name + edge ID
+    ResolveNeighborsInner(Vid, Arc<str>, Eid), // same as ResolveNeighbors
+    ResolveCoercion(Vid, Arc<str>, Arc<str>),  // vertex ID + current type + coerced-to type
 }
 
 struct PerfSpanIter<I, T, F>
@@ -214,7 +97,7 @@ where
 pub struct TracingAdapter<'vertex, AdapterT>
 where
     AdapterT: Adapter<'vertex>,
-    AdapterT::Vertex: VertexT + 'vertex,
+    AdapterT::Vertex: Debug + Clone + 'vertex,
 {
     pub tracer: Rc<RefCell<Tracer>>,
     inner: AdapterT,
@@ -224,7 +107,7 @@ where
 impl<'vertex, AdapterT> TracingAdapter<'vertex, AdapterT>
 where
     AdapterT: Adapter<'vertex>,
-    AdapterT::Vertex: VertexT + 'vertex,
+    AdapterT::Vertex: Debug + Clone + 'vertex,
 {
     pub fn new(adapter: AdapterT, tracer: Rc<RefCell<Tracer>>) -> Self {
         Self {
@@ -246,7 +129,7 @@ where
 impl<'vertex, AdapterT> Adapter<'vertex> for TracingAdapter<'vertex, AdapterT>
 where
     AdapterT: Adapter<'vertex> + 'vertex,
-    AdapterT::Vertex: VertexT + 'vertex,
+    AdapterT::Vertex: Debug + Clone + 'vertex,
 {
     type Vertex = AdapterT::Vertex;
 
@@ -256,30 +139,10 @@ where
         parameters: &EdgeParameters,
         resolve_info: &ResolveInfo,
     ) -> VertexIterator<'vertex, Self::Vertex> {
-        let mut trace = self.tracer.borrow_mut();
-        // Even if we don't record the function call, there still needs to be some
-        // way of distinguishing between calls.
-        let call_opid = trace.record(
-            TraceOpType::Call(Box::new(FunctionCall::ResolveStartingVertices(
-                resolve_info.vid(),
-            ))),
-            None,
-            None,
-        );
-        drop(trace);
-
-        let inner_iter = self
-            .inner
-            .resolve_starting_vertices(edge_name, parameters, resolve_info);
-        let tracer_ref_1 = self.tracer.clone();
-        Box::new(make_iter_with_perf_span(inner_iter, move |v, d| {
-            tracer_ref_1.borrow_mut().record(
-                TraceOpType::YieldFrom(YieldValue::ResolveStartingVertices),
-                Some(call_opid),
-                Some(d),
-            );
-            v
-        }))
+        // We don't care about the first resolution since it's normally quick
+        // and it only occurs once.
+        self.inner
+            .resolve_starting_vertices(edge_name, parameters, resolve_info)
     }
 
     fn resolve_property<V: AsVertex<Self::Vertex> + 'vertex>(
@@ -289,32 +152,36 @@ where
         property_name: &Arc<str>,
         resolve_info: &ResolveInfo,
     ) -> ContextOutcomeIterator<'vertex, V, FieldValue> {
-        let mut trace = self.tracer.borrow_mut();
-        let call_opid = trace.record(
-            TraceOpType::Call(Box::new(FunctionCall::ResolveProperty(
-                resolve_info.vid(),
-                type_name.clone(),
-                property_name.clone(),
-            ))),
-            None,
-            None,
+        // For each resolution we want to know:
+        // 1. What are we resolving?
+        // 2. How long did the resolution take?
+        // 3. How many times did we resolve the same property?
+        //
+        // We are not collecting:
+        // 1. Number of times a function is called.
+        // 2. Whether or not any results were returned.
+
+        let call_id = FunctionCall::ResolveProperty(
+            resolve_info.vid(),
+            type_name.clone(),
+            property_name.clone(),
         );
-        drop(trace);
 
         let inner_iter =
             self.inner
                 .resolve_property(contexts, type_name, property_name, resolve_info);
 
-        let tracer_ref_4 = self.tracer.clone();
+        let tracer_ref = self.tracer.clone();
 
-        Box::new(make_iter_with_perf_span(inner_iter, move |(context, value), d| {
-            tracer_ref_4.borrow_mut().record(
-                TraceOpType::YieldFrom(YieldValue::ResolveProperty),
-                Some(call_opid),
-                Some(d),
-            );
-            (context, value)
-        }))
+        Box::new(make_iter_with_perf_span(
+            inner_iter,
+            move |(context, value), duration| {
+                tracer_ref
+                    .borrow_mut()
+                    .record_time(call_id.clone(), duration);
+                (context, value)
+            },
+        ))
     }
 
     fn resolve_neighbors<V: AsVertex<Self::Vertex> + 'vertex>(
@@ -325,48 +192,58 @@ where
         parameters: &EdgeParameters,
         resolve_info: &ResolveEdgeInfo,
     ) -> ContextOutcomeIterator<'vertex, V, VertexIterator<'vertex, Self::Vertex>> {
-        let mut trace = self.tracer.borrow_mut();
-        let call_opid = trace.record(
-            TraceOpType::Call(Box::new(FunctionCall::ResolveNeighbors(
-                resolve_info.origin_vid(),
-                type_name.clone(),
-                resolve_info.eid(),
-            ))),
-            None,
-            None,
+        // Along with the standard information, we also want to know
+        // how many times each inner iterator yielded.
+        //
+        // While in most cases the time spent in inner iterators will be
+        // overshadowed by time spent in outer iterators, it can be significant.
+        //
+        // inner and outer call times are often quite different, so they need to
+        // be stored differently.
+        let call_id = FunctionCall::ResolveNeighbors(
+            resolve_info.origin_vid(),
+            type_name.clone(),
+            resolve_info.eid(),
         );
-        drop(trace);
+        let call_id_inner = FunctionCall::ResolveNeighborsInner(
+            resolve_info.origin_vid(),
+            type_name.clone(),
+            resolve_info.eid(),
+        );
 
         let inner_iter =
             self.inner
                 .resolve_neighbors(contexts, type_name, edge_name, parameters, resolve_info);
 
-        let tracer_ref_5 = self.tracer.clone();
+        let tracer_ref = self.tracer.clone();
 
-        Box::new(make_iter_with_perf_span(inner_iter, move |(context, neighbor_iter), d| {
-            let mut trace = tracer_ref_5.borrow_mut();
-            let outer_iterator_opid = trace.record(
-                TraceOpType::YieldFrom(YieldValue::ResolveNeighborsOuter),
-                Some(call_opid),
-                Some(d),
-            );
-            drop(trace);
+        Box::new(make_iter_with_perf_span(
+            inner_iter,
+            move |(context, neighbor_iter), duration| {
+                tracer_ref
+                    .borrow_mut()
+                    .record_time(call_id.clone(), duration);
 
-            let tracer_ref_6 = tracer_ref_5.clone();
-            let tapped_neighbor_iter = Box::new(
-                make_iter_with_perf_span(neighbor_iter.enumerate(), move |(pos, vertex), d| {
-                    tracer_ref_6.borrow_mut().record(
-                        TraceOpType::YieldFrom(YieldValue::ResolveNeighborsInner),
-                        Some(outer_iterator_opid),
-                        Some(d),
-                    );
-                    (pos, vertex)
-                })
-                .map(move |(_, vertex)| vertex),
-            );
+                let tracer_ref_2 = tracer_ref.clone();
 
-            (context, tapped_neighbor_iter)
-        }))
+                let value = call_id_inner.clone();
+
+                let tapped_neighbor_iter = Box::new(
+                    make_iter_with_perf_span(
+                        neighbor_iter.enumerate(),
+                        move |(pos, vertex), duration| {
+                            tracer_ref_2
+                                .borrow_mut()
+                                .record_time(value.clone(), duration);
+                            (pos, vertex)
+                        },
+                    )
+                    .map(|(_, vertex)| vertex),
+                );
+
+                (context, tapped_neighbor_iter)
+            },
+        ))
     }
 
     fn resolve_coercion<V: AsVertex<Self::Vertex> + 'vertex>(
@@ -376,32 +253,24 @@ where
         coerce_to_type: &Arc<str>,
         resolve_info: &ResolveInfo,
     ) -> ContextOutcomeIterator<'vertex, V, bool> {
-        let mut trace = self.tracer.borrow_mut();
-        let call_opid = trace.record(
-            TraceOpType::Call(Box::new(FunctionCall::ResolveCoercion(
-                resolve_info.vid(),
-                type_name.clone(),
-                coerce_to_type.clone(),
-            ))),
-            None,
-            None,
+        let call_id = FunctionCall::ResolveCoercion(
+            resolve_info.vid(),
+            type_name.clone(),
+            coerce_to_type.clone(),
         );
-        drop(trace);
 
         let inner_iter =
             self.inner
                 .resolve_coercion(contexts, type_name, coerce_to_type, resolve_info);
 
-        let tracer_ref_5 = self.tracer.clone();
+        let tracer_ref = self.tracer.clone();
 
         Box::new(make_iter_with_perf_span(
             inner_iter,
-            move |(context, can_coerce), d| {
-                tracer_ref_5.borrow_mut().record(
-                    TraceOpType::YieldFrom(YieldValue::ResolveCoercion),
-                    Some(call_opid),
-                    Some(d),
-                );
+            move |(context, can_coerce), duration| {
+                tracer_ref
+                    .borrow_mut()
+                    .record_time(call_id.clone(), duration);
                 (context, can_coerce)
             },
         ))
